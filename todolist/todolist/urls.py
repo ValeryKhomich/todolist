38e74c5db86f202390374de5d3b823e135ea4806
Valery <KhomichValery.gmail.com>
2024-06-24 08:49:06 +0300

    new

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #comment123
+    path('admin/', admin.site.urls),
+    #path('', admin.site.urls),
 
 ]
